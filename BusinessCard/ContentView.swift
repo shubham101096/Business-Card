6b19af9c3704266fde527473d0604b955ab422f2
Shubham Mishra <shubhammishra10@gmail.com>
2021-05-05 12:04:48 +0530

    ADD: Profile image, name, contact no. using swift ui

--- BusinessCard/ContentView.swift
+++ BusinessCard/ContentView.swift
@@ -9,13 +9,52 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color(red: 0.15, green: 0.68, blue: 0.38)
+                .ignoresSafeArea()
+            VStack {
+                Image("profilePic")
+                    .resizable()
+                    .frame(width: 150, height: 150, alignment: .center)
+                    .aspectRatio(contentMode: .fit)
+                    .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                    .overlay(
+                        Circle().stroke(Color.white, lineWidth: 3)
+                    )
+                Text("Shubham Mishra")
+                    .font(.custom("Pacifico-Regular", size: 40))
+                    .fontWeight(.bold)
+                    .foregroundColor(.white)
+                Text("iOS Developer")
+                    .font(.system(size: 25))
+                    .foregroundColor(.white)
+                Divider()
+                InfoView(text: "+91 999 999 9999", imageName: "phone.circle")
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct InfoView: View {
+    let text: String
+    let imageName: String
+    var body: some View {
+        RoundedRectangle(cornerRadius: 25)
+            .fill(Color.white)
+            .frame(height: 50)
+            .overlay(
+                HStack {
+                    Image(systemName: imageName)
+                        .foregroundColor(.green)
+                    Text(text)
+                }
+            )
+            .padding()
     }
 }
