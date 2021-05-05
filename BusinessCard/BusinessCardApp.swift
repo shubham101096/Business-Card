434c730eb449f3005aa5725fa9881c3c1a60414c
Shubham Mishra <shubhammishra10@gmail.com>
2021-05-05 10:39:03 +0530

    Initial Commit

---
+++ BusinessCard/BusinessCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  BusinessCardApp.swift
+//  BusinessCard
+//
+//  Created by Shubham Mishra on 05/05/21.
+//
+
+import SwiftUI
+
+@main
+struct BusinessCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
